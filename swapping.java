3cf2f5462727992f30a7921e0e9854eaaa3451cc
hussain94413 <113414777+hussain94413@users.noreply.github.com>
2023-07-25 18:18:40 +0530

    Create swapping.java

---
+++ swapping.java
@@ -0,0 +1,20 @@
+class Swap
+{
+    public static void main(String[] args)
+    {
+        int x = 10;
+        int y = 12;
+
+        System.out.println("Before Swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("After swap");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
