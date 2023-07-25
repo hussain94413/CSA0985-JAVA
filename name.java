42dc543c061052c6549f4428c5eced6564e26c5d
hussain94413 <113414777+hussain94413@users.noreply.github.com>
2023-07-25 18:05:36 +0530

    Create name.java

---
+++ name.java
@@ -0,0 +1,10 @@
+class hussain
+{
+    public static void main(String[] args)
+    {
+        int x = 25;
+        int y = 15;
+        int z = x+y;
+        System.out.println(x + "+" + y + "=" + z);
+    }
+}
