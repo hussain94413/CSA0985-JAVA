0914550370647934968123954df645ed56b6febd
hussain94413 <113414777+hussain94413@users.noreply.github.com>
2023-07-25 18:11:09 +0530

    Create odd or even.java

---
+++ odd or even.java
@@ -0,0 +1,15 @@
+class even
+{
+    public static void main(String[] args)
+    {
+        int a =20;
+        if(a%2==0)
+        {
+            System.out.println("a is Even");
+        }
+        else
+        {
+            System.out.println("a is Odd");
+        }
+    }
+}    
