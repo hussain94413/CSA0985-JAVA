f5bb9ee532ac1d6cf372cdb430d5b0e3b029c721
hussain94413 <113414777+hussain94413@users.noreply.github.com>
2023-07-25 18:10:29 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,15 @@
+class leap
+{
+    public static void main(String[] args)
+    {
+        int a =2021;
+        if(a%4==0)
+        {
+            System.out.println("a is Leap year");
+        }
+        else
+        {
+            System.out.println("a is Non-leap year");
+        }
+    }
+}    
