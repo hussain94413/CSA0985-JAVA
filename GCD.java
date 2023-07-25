a8a9e929be922b70fae3da79f86211e06448cce9
hussain94413 <113414777+hussain94413@users.noreply.github.com>
2023-07-25 18:08:16 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,13 @@
+class gcd
+{
+    public static void main(String[] args)
+    {
+        int num1=12,num2=8,i,gcd=1;
+        for(i=1;i<=num1 && i<=num2;i++)
+        {
+            if(num1%i==0 && num2==0);
+            gcd=i;
+        }
+        System.out.println("GCD:"+gcd);
+    }
+}
