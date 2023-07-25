98973ee7d69e85f0f8b2c93b6e6355dfefacf99b
hussain94413 <113414777+hussain94413@users.noreply.github.com>
2023-07-25 18:12:01 +0530

    Create quoitient and remainder.java

---
+++ quoitient and remainder.java
@@ -0,0 +1,11 @@
+class remainder
+{
+    public static void main(String[] args)
+    {
+        int dividend=50,divisor=2,quotient,remainder;
+        quotient=dividend/divisor;
+        System.out.println("Quotient:"+quotient);
+        remainder=dividend%divisor;
+        System.out.println("Remainder:"+remainder);
+    }
+}
